f6fdd751da230e3d960a43eba6f4b51e1d790eea
Ashish_Aman <128075752+Ashish-aman@users.noreply.github.com>
2025-03-24 01:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-TWILIO_ACCOUNT_SID = os.getenv(TWILIO_ACCOUNT_SID)
-TWILIO_AUTH_TOKEN = os.getenv(TWILIO_AUTH_TOKEN)
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 
 # client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 def open_link(url):
